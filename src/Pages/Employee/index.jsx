00c645fdee26385a3a344651adbefae2fc0e22c0
CharlesVeneau <veneauc@hotmail.fr>
2022-08-27 23:31:47 +0200

    main style of right column

--- src/Pages/Employee/index.jsx
+++ src/Pages/Employee/index.jsx
@@ -1,10 +1,12 @@
 function Employee() {
   return (
-    <form action="">
-      <label htmlFor="firstname">First Name</label>
-      <input type="text" name="firstname" id="firstname" required />
-      <button type="submit">Save</button>
-    </form>
+    <article className="col-span-3 flex justify-center align-middle my-4 mx-8">
+      <form action="" className="bg-white rounded-lg w-4/5 shadow">
+        <label htmlFor="firstname">First Name</label>
+        <input type="text" name="firstname" id="firstname" required />
+        <button type="submit">Save</button>
+      </form>
+    </article>
   );
 }
 
