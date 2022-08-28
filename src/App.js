1a1fbc7981186397cde15dba48b67043542102d6
CharlesVeneau <veneauc@hotmail.fr>
2022-08-28 22:51:53 +0200

    Create Navbar component, add active link classes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import Logo from './assets/logoPNG.png';
 import { Routes, Route } from 'react-router-dom';
-import { UserAddIcon } from '@heroicons/react/outline';
-import { UserGroupIcon } from '@heroicons/react/outline';
-import { NavLink } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 import Employee from './Pages/Employee';
 import Employees from './Pages/Employees';
 
 function App() {
   return (
-    <div className="App grid grid-cols-4 h-screen bg-slate-300">
+    <div className="App grid grid-cols-4 h-screen bg-slate-200">
       {/* What I want?
      2 columns
      the left one contains a header with the logo and the name of the website (HRNet)
@@ -18,41 +15,11 @@ function App() {
      the second to the employee's list of
 
      The right column will display either the new employee form or the employee's list */}
-      <aside className="col-span-1 p-2 pt-4">
-        <h1 className="text-3xl font-bold text-gray-700 flex align-center gap-2 mb-6 ">
-          <img
-            src={Logo}
-            alt="WealthHealt"
-            className="h-10 w-10 inline align-middle "
-          />
-          HRNet
-        </h1>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className="inline-flex text-gray-700 align-center gap-2 p-2 pr-4 mb-3 hover:bg-slate-50 hover:text-green-600 rounded-md active:bg-white active:text-green-600 transition-colors"
-            >
-              <UserAddIcon className="h-6 w-6 inline" /> New employee
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="employees"
-              className="inline-flex text-gray-700 align-center gap-2 p-2 pr-4 mb-3 hover:bg-slate-50 hover:text-green-600 rounded-md active:bg-white active:text-green-600 transition-colors"
-            >
-              <UserGroupIcon className="h-6 w-6 inline" />
-              Employees list
-            </NavLink>
-          </li>
-        </ul>
-      </aside>
-      <article className="col-span-3">
-        <Routes>
-          <Route path="" element={<Employee />} />
-          <Route path="/employees" element={<Employees />} />
-        </Routes>
-      </article>
+      <Navbar />
+      <Routes>
+        <Route path="" element={<Employee />} />
+        <Route path="/employees" element={<Employees />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,48 @@
+import Logo from '../assets/logoPNG.png';
+import { UserAddIcon } from '@heroicons/react/outline';
+import { UserGroupIcon } from '@heroicons/react/outline';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <aside className="col-span-1 p-2 pt-4">
+      <h1 className="text-3xl font-bold text-gray-900 flex align-center gap-2 mb-6 ">
+        <img
+          src={Logo}
+          alt="WealthHealt"
+          className="h-10 w-10 inline align-middle "
+        />
+        HRNet
+      </h1>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? 'flex align-center gap-2 p-2 pr-4 mb-3 hover:bg-slate-100/90 hover:text-green-600 rounded-md transition-colors bg-slate-100/90 text-green-600'
+                : 'flex align-center gap-2 p-2 pr-4 mb-3 hover:bg-slate-100/90 hover:text-green-600 rounded-md transition-colors text-gray-700'
+            }
+          >
+            <UserAddIcon className="h-6 w-6 inline" /> New employee
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="employees"
+            className={({ isActive }) =>
+              isActive
+                ? 'flex align-center gap-2 p-2 pr-4 mb-3 hover:bg-slate-100/90 hover:text-green-600 rounded-md transition-colors bg-slate-100/90 text-green-600'
+                : 'flex align-center gap-2 p-2 pr-4 mb-3 hover:bg-slate-100/90 hover:text-green-600 rounded-md transition-colors text-gray-700'
+            }
+          >
+            <UserGroupIcon className="h-6 w-6 inline" />
+            Employees list
+          </NavLink>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Navbar;
