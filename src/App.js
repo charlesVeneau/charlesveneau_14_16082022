34ccfe019338831b1c57da6315a49f82e6aa5d2e
CharlesVeneau <veneauc@hotmail.fr>
2022-09-07 20:55:29 +0200

    Clean comment from app file

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import Employees from './Pages/Employees';
 function App() {
   return (
     <div className="App grid grid-cols-4 bg-slate-200">
-      {/* What I want?
-     2 columns
-     the left one contains a header with the logo and the name of the website (HRNet)
-     A vertical list of two Navlinks
-     the first one to the create employee
-     the second to the employee's list of
-
-     The right column will display either the new employee form or the employee's list */}
       <Navbar />
       <Routes>
         <Route path="" element={<Form />} />
