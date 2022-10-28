fbdbce5a2caff806574373f5f97bed3a7b571594
CharlesVeneau <veneauc@hotmail.fr>
2022-10-28 11:47:06 +0200

    Refactor block of css to css file

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,36 +3,27 @@ import { UserAddIcon, UserGroupIcon } from '@heroicons/react/outline';
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
-  const link =
-      'flex align-center gap-2 p-2 md:pr-4 xl:mb-3 hover:bg-slate-100/90 hover:text-green-600 rounded-md transition-colors bg-slate-100/90 text-green-600 justify-center md:justify-start',
-    activeLink =
-      'flex align-center gap-2 p-2 md:pr-4 xl:mb-3 hover:bg-slate-100/90 hover:text-green-600 rounded-md transition-colors text-gray-700 justify-center md:justify-start';
-
   return (
     <aside className="navbar">
-      <h1 className="text-center hidden sm:text-2xl md:text-3xl sm:flex sm:align-center gap-2 xl:mb-6 font-bold text-gray-900 ">
-        <img
-          src={Logo}
-          alt="WealthHealt"
-          className=" h-8 md:h-10 inline align-middle "
-        />
-        <span className="">HRNet</span>
-      </h1>
+      <NavLink to="form">
+        <h1 className="text-center hidden sm:text-2xl md:text-3xl sm:flex sm:align-center gap-2 xl:mb-6 font-bold text-gray-900 ">
+          <img
+            src={Logo}
+            alt="WealthHealt"
+            className=" h-8 md:h-10 inline align-middle "
+          />
+          <span className="">HRNet</span>
+        </h1>
+      </NavLink>
       <ul className="flex xl:block justify-center gap-3">
         <li className="flex-grow">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? link : activeLink)}
-          >
+          <NavLink to="form">
             <UserAddIcon className="h-6 w-6 inline" />{' '}
             <span className="">New employee</span>
           </NavLink>
         </li>
         <li className="flex-grow">
-          <NavLink
-            to="employees"
-            className={({ isActive }) => (isActive ? link : activeLink)}
-          >
+          <NavLink to="employees">
             <UserGroupIcon className="h-6 w-6 inline" />
             <span className="">Employees list</span>
           </NavLink>
