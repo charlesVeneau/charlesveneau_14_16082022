a33ee5644862acc0adc69a91545447e51c26540c
CharlesVeneau <veneauc@hotmail.fr>
2022-11-24 11:11:09 +0100

    set hashrouter in order to get a static website

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { ModalProvider, UsersProvider } from './utils/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <UsersProvider>
       <ModalProvider>
         <App />
       </ModalProvider>
     </UsersProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
